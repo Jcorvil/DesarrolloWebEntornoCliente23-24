47254e0adc6e2283bd5b55b214a7f3363f7c0578
unknown <jcorvil600@g.educaand.es>
2023-11-23 10:35:58 +0100

    23/11/2023. Se sigue actividad cesta

--- Tema8/Actividades/Ejercicio1/script.js
+++ Tema8/Actividades/Ejercicio1/script.js
@@ -1,8 +1,22 @@
-var productos = [
-    [1, "Bolso", 20, "https://source.unsplash.com/random/500x500?purse"],
-    [2, "Móvil", 120, "https://source.unsplash.com/random/500x500?smartphone"],
-    [3, "Taza", 10, "https://source.unsplash.com/random/500x500?mug"],
-    [4, "Zapatos", 50.5, "https://source.unsplash.com/random/500x500?shoes"],
-    [5, "Vestido", 30, "https://source.unsplash.com/random/500x500?dress"],
-    [6, "Cámara", 100, "https://source.unsplash.com/random/500x500?camera"],
-]
+class Cesta{
+    
+}
+
+class Productos{
+
+    
+    productos = [
+        [1, "Bolso", 0, 20, "https://source.unsplash.com/random/500x500?handbag"],
+        [2, "Móvil", 0, 120, "https://source.unsplash.com/random/500x500?smartphone"],
+        [3, "Taza", 0, 10, "https://source.unsplash.com/random/500x500?mug"],
+        [4, "Zapatos", 0, 50.5, "https://source.unsplash.com/random/500x500?shoes"],
+        [5, "Vestido", 0, 30, "https://source.unsplash.com/random/500x500?dress"],
+        [6, "Cámara", 0, 100, "https://source.unsplash.com/random/500x500?camera"],
+    ]
+    
+}
+
+
+class ArrayProductos{
+
+}
