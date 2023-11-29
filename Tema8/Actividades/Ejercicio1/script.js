ff4d1414d520c5d107c8baf378d1fa578b38fd81
unknown <jcorvil600@g.educaand.es>
2023-11-29 13:14:17 +0100

    29/11/2023. Se sigue actividad cesta

--- Tema8/Actividades/Ejercicio1/script.js
+++ Tema8/Actividades/Ejercicio1/script.js
@@ -1,22 +1,75 @@
-class Cesta{
-    
+class Cesta {}
+
+class Productos {
+	constructor(id, nombre, cantidad, precio, imagen) {
+		this.id = id;
+		this.nombre = nombre;
+		this.cantidad = cantidad;
+		this.precio = precio;
+		this.imagen = imagen;
+	}
 }
 
-class Productos{
+class ContProductos {
+	productos = [
+		{
+			id: 1,
+			nombre: "Bolso",
+			cantidad: 0,
+			precio: 20,
+			imagen: "https://source.unsplash.com/random/500x500?handbag",
+		},
+		{
+			id: 2,
+			nombre: "Móvil",
+			cantidad: 0,
+			precio: 120,
+			imagen: "https://source.unsplash.com/random/500x500?smartphone",
+		},
+		{
+			id: 3,
+			nombre: "Taza",
+			cantidad: 0,
+			precio: 10,
+			imagen: "https://source.unsplash.com/random/500x500?mug",
+		},
+		{
+			id: 4,
+			nombre: "Zapatos",
+			cantidad: 0,
+			precio: 50.5,
+			imagen: "https://source.unsplash.com/random/500x500?shoes",
+		},
+		{
+			id: 5,
+			nombre: "Vestido",
+			cantidad: 0,
+			precio: 30,
+			imagen: "https://source.unsplash.com/random/500x500?dress",
+		},
+		{
+			id: 6,
+			nombre: "Cámara",
+			cantidad: 0,
+			precio: 100,
+			imagen: "https://source.unsplash.com/random/500x500?camera",
+		},
+	];
 
-    
-    productos = [
-        [1, "Bolso", 0, 20, "https://source.unsplash.com/random/500x500?handbag"],
-        [2, "Móvil", 0, 120, "https://source.unsplash.com/random/500x500?smartphone"],
-        [3, "Taza", 0, 10, "https://source.unsplash.com/random/500x500?mug"],
-        [4, "Zapatos", 0, 50.5, "https://source.unsplash.com/random/500x500?shoes"],
-        [5, "Vestido", 0, 30, "https://source.unsplash.com/random/500x500?dress"],
-        [6, "Cámara", 0, 100, "https://source.unsplash.com/random/500x500?camera"],
-    ]
-    
+	mostrarProductos() {
+		const tablaProductos = document.getElementById("tablaProductos");
+
+		this.productos.forEach((producto) => {
+			const tr = document.createElement("tr");
+			tr.innerHTML = `
+                <td><img src="${producto.imagen}" alt="${producto.nombre}" class="producto__foto"></td>
+                <td>${producto.nombre}</td>
+                <td>${producto.precio} €</td>
+                <td><input type="number" placeholder="Cantidad"></td>
+                <td><button onclick="ContProductos.agregarALaCesta(${producto.id})">Añadir</button></td>`;
+			productosBody.appendChild(tr);
+		});
+	}
 }
 
-
-class ArrayProductos{
-
-}
+const tienda = new Productos();
