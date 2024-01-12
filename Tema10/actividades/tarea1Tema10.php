672023b14d19b473a62addf4e9b351280453e3f0
unknown <jcorvil600@g.educaand.es>
2024-01-12 10:34:24 +0100

    12/01/2024. Se avanza la actividad

--- Tema10/actividades/tarea1Tema10.php
+++ Tema10/actividades/tarea1Tema10.php
@@ -3,25 +3,30 @@
 function getDatos($id)
 {
     try {
-        $usuario = 'root';
-        $contraseña = '';
+        // Conexión con la BBDD
+        $conexion = new PDO('mysql:host=localhost;dbname=tema10', 'root', '');
+        // Sentencia para obtener todos los datos de la id
+        $sql = "SELECT * FROM datos";
+        if ($id != null) {
+            $sql .= " WHERE id = :id";
+        }
 
-        $conexion = new PDO('mysql:host=localhost;dbname=tema10', $usuario, $contraseña);
-        $sql = "SELECT * FROM datos WHERE id = :id";
         $pdost = $conexion->prepare($sql);
+        // Bindeamos para evitar la inyección de código
         $pdost->bindParam(':id', $id, PDO::PARAM_INT);
         $pdost->setFetchMode(PDO::FETCH_ASSOC);
 
         $pdost->execute();
 
-        $result = $pdost->fetch();
+        //Devuelve los datos
+        $result = $pdost->fetchAll();
         echo json_encode($result);
 
     } catch (PDOException $e) {
         echo "Error: " . $e->getMessage();
     }
 }
-
+// Si la id existe, ejecuta la función y devuelve los datos
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
     getDatos($id);
