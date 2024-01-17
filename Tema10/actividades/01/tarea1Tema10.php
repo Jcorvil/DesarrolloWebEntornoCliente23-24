345c1a05305c296ecbc4ff102bfebdd476449ecb
Jorge <jcorvil600@g.educaand.es>
2024-01-17 16:48:44 +0100

    Actividad 01

---
+++ Tema10/actividades/01/tarea1Tema10.php
@@ -0,0 +1,29 @@
+<?php
+
+// Conexión con la BBDD
+$conexion = new PDO('mysql:host=localhost;dbname=tema10', 'root', '');
+
+$id = $_GET['id'] ?? null;
+
+// Sentencia para obtener todos los datos de la id
+$sql = "SELECT * FROM datos";
+
+// Si la id existe, añade la condición WHERE a la consulta
+if ($id != null) {
+  $sql .= " WHERE id = :id";
+}
+
+$pdost = $conexion->prepare($sql);
+
+// Bindeamos para evitar la inyección de código
+if ($id != null) {
+  $pdost->bindParam(':id', $id, PDO::PARAM_INT);
+}
+
+$pdost->execute();
+
+// Devuelve los datos
+$result = $pdost->fetchAll(PDO::FETCH_ASSOC);
+echo json_encode($result);
+ 
+?>
